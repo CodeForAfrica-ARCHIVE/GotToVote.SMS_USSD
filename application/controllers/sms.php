b3f51fa069a2bf94f99799829971188ce2139195
Nick Hargreaves <hargreaves1234@gmail.com>
2013-10-31 15:36:17 +0300

    show 5 more

--- application/controllers/sms.php
+++ application/controllers/sms.php
@@ -7,20 +7,43 @@ class SMS extends CI_Controller {
 		 }
 		public function index(){
 			$sms = $_GET['message'];
-			$number = $_GET['number'];
+			$number = $_GET['number'];	
 			
-			if(trim($sms)==""){
-				$counties = $this->config->item('counties');
-				$c = '';
-				foreach($counties as $k=>$v){
-					$c .= $k.":".$v."\n"; 				
-				}
-				$this->send_response("Reply with a county number:\n".$c, $number);			
-			}else{	
-				$sms = $this->sms_functions->clean($sms);
-			}	
+			//if message = More, retain level and load more
+			if(strtoupper($message)=="M"){
+				$level = $this->sms_functions->get_level($number);
+				$screen = $this->sms_functions->get_screen($screen);
+				$this->sms_functions->next_screen($number, $level, $screen);			
+			}else{
+				if(trim($sms)==""){
+					$this->load_counties($number, 1);	
+						
+				}else{	
+					$sms = $this->sms_functions->clean($sms);
+				}	
+			}
+		}
+		public function next_screen($number, $level, $screen){
+			$newscreen = $screen + 1;
+			if($level==1){
+				$this->load_counties($number, $newscreen);			
+			}		
+		}
+		public function load_counties($number, $newscreen){
+			$counties = $this->config->item('counties');
+			$c = '';
+			$i = 5 ;
+			foreach($counties as $k=>$v){
+								
+				if($i<($newscreen*5)){					
+					$c .= $k.":".$v."\n";
+				} 
+				$i++;				
+			}
+			$this->send_response("Reply with a county number:\n".$c."\nM:More", $number);	
 		}
 		public function send_response($message, $number){
+			$this->sms_functions->dblog($message, $number);			
 			/*
 			$api_url = $this->config->item('api_url');
 			$api_key = $this->config->item('api_key');
--- application/models/sms_functions.php
+++ application/models/sms_functions.php
@@ -15,6 +15,48 @@ class Sms_functions extends CI_Model {
 			$sms = strtolower($sms);
 			$sms = ucfirst($sms);
 			return $sms;		
-		}	
+		}
+		public function dblog($message, $number){
+			//check if there is active session
+			$this->db->select("*");
+			$this->db->from("sessions");
+			$this->db->where("sess_number", $number);
+			$query = $this->db->get();
+			$total = $query->num_rows();
+			
+			$query = $query->result_array();
+			$query = $query[0];	
+			if($total>0){
+			//check session level	
+				$level = $query['sess_level'];
+							
+			}else{
+			//start new session
+				$this->db->query("insert into sessions(sess_number, sess_level, sess_current_screen, sess_started)values('$number', '1', '1', now())");				
+			}
+		}
+		public function get_level($number){
+			$this->db->select("*");
+			$this->db->from("sessions");
+			$this->db->where("sess_number", $number);
+			$query = $this->db->get();
+			
+			$query = $query->result_array();
+			$query = $query[0];	
+			$level = $query['sess_level'];
+			return $level;	
+		}
+		public function get_screen($number){
+			$this->db->select("*");
+			$this->db->from("sessions");
+			$this->db->where("sess_number", $number);
+			$query = $this->db->get();
+			
+			$query = $query->result_array();
+			$query = $query[0];	
+			$screen = $query['sess_current_screen'];
+			return $screen;	
+		}
+	
 }
 ?>
